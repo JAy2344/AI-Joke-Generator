bff1f876b884fccea2b5ce1a58053245b1675c04
Ayanda Dlamini <Janicedoe745@gmail.com>
2025-08-28 14:51:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function tellJoke(response) {
   function generateJoke() {
 
     let APIKey = "ad28f3a0557d8t5f574o89b184356e5a";
-    let context = "You are a helpful assistant that generates IT Support , computers and everything IT jokes. create a short, funny, and clean joke.basic HTML and separate each line with a <br /> do not include ```html and show me different jokes everytime i ask;";
+    let context = "You are a helpful assistant that generates IT Support , computers and everything IT jokes.be creative and funnier every time i ask create a short, funny, and clean joke.basic HTML and separate each line with a <br /> do not include ```html and show me different jokes everytime i ask;";
     let prompt = "Tell me a IT support Joke tell me a different joke each time I ask.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${APIKey}`;
 
