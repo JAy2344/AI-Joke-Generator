27e1aa19b5d6ae4f09ca752b7f66d69fe2cec7b0
Ayanda Dlamini <Janicedoe745@gmail.com>
2025-09-09 16:11:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ function tellJoke(response) {
     new Typewriter(jokeElement, {
       strings: response.data.answer,
       autoStart: true,
+      Delay:1,
+      
     });
   }
 
   function generateJoke() {
 
     let APIKey = "ad28f3a0557d8t5f574o89b184356e5a";
-    let context = "You are a helpful assistant that generates IT Support , computers and everything IT jokes.be creative and funnier every time i ask create a short, funny, and clean joke.basic HTML and separate each line with a <br /> do not include ```html and show me different jokes everytime i ask;";
-    let prompt = "Tell me a IT support Joke tell me a different joke each time I ask.";
+    let context = "You are a helpful assistant that generates IT and everything comptures make them dad joke quality and corny.be creative and funnier every time i ask create a short, funny, and clean joke.basic HTML do not include ```html and show me different puns everytime i ask.dont include this Sure! Here’s a classic for you: ;";
+    let prompt = "Generate IT dad joke IT related jokes.different jokes each time I ask.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${APIKey}`;
 
     jokeElement.innerHTML = "⏳Finding the funniest bad joke... please wait";
